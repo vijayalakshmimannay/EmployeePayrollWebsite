29c515b995eed5551563ac43d68631b5db4acb37
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-07-13 15:45:27 +0530

    [vijayalakshmi] validation of name and date and create employee payroll data on save

---
+++ employee-payroll-jsonserver/js/EmployeePayroll.js
@@ -0,0 +1,78 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    validateName();
+    salaryRange();
+})
+
+function validateName() {
+    const name = document.querySelector('#name');
+    const nameError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        try {
+            let empData = new EmployeePayroll();
+            empData.name = name.value;
+            nameError.textContent = "";
+        } catch (e) {
+            nameError.textContent = e;
+        }
+    });
+}
+
+function salaryRange() {
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
+}
+
+const save = () => {
+    let employeePayroll = createEmployeePayroll();
+    alert(JSON.stringify(employeePayroll));
+}
+
+const createEmployeePayroll = () => {
+    let employeePayroll = new EmployeePayroll();
+    try {
+        employeePayroll.name = getInputValueId("#name");
+        setTextValue('.text-error', "");
+    } catch (e) {
+        setTextValue('.text-error', e);
+    }
+
+    try {
+        let date = getInputValueId('#day') + " " + getInputValueId('#month') + " " + getInputValueId('#year');
+        employeePayroll.startDate = new Date(Date.parse(date));
+        setTextValue('.date-error', "");
+    } catch (e) {
+        setTextValue('.date-error', e);
+    }
+
+    employeePayroll.profilePic = getSelectedValue('[name=profile]').pop();
+    employeePayroll.gender = getSelectedValue('[name=gender]').pop();
+    employeePayroll.department = getSelectedValue('[name=department]');
+    employeePayroll.salary = getInputValueId('#salary');
+    employeePayroll.notes = getInputValueId('#notes');
+    employeePayroll.id = new Date().getTime()+1;
+    return employeePayroll;
+}
+
+const getInputValueId = (id) => {
+    return document.querySelector(id).value;
+}
+
+const setTextValue = (id, message) => {
+    const textError = document.querySelector(id);
+    textError.textContent = message;
+}
+
+const getSelectedValue = (propertyValue)=> {
+    let allItem = document.querySelectorAll(propertyValue);
+    let setItem = [];
+    allItem.forEach(item=>{
+        if(item.checked == true){
+            setItem.push(item.value);
+        }
+    })
+    return setItem;
+}
---
+++ employee-payroll-jsonserver/js/Payroll.js
@@ -0,0 +1,71 @@
+class EmployeePayroll {
+    //getter and setter method
+
+    get id(){
+        return this._id;
+    }
+    set id(id){
+        this._id=id;
+    }
+     
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        console.log("inside setter");
+        const nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if (nameRegex.test(name)) {
+            this._name=name;
+        } else {
+            throw "Name should start with capital letter and have atleast 3 characters";
+        }
+    }
+
+    get profilePic(){
+        return this._profilePic;
+    }
+    set profilePic(profilePic){
+        this._profilePic=profilePic;
+    }
+
+    get gender(){
+        return this._gender;
+    }
+    set gender(gender){
+        this._gender=gender;
+    }
+
+    get department(){
+        return this._department;
+    }
+    set department(department){
+        this._department=department;
+    }
+
+    get salary(){
+        return this._salary;
+    }
+    set salary(salary){
+        this._salary=salary;
+    }
+
+    get notes(){
+        return this._notes;
+    }
+    set notes(notes){
+        this._notes=notes;
+    }
+
+    get startDate(){
+        return this._startDate;
+    }
+    set startDate(startDate){
+        let currentDate = new Date();
+        if(startDate <= currentDate){
+            this._startDate = startDate;
+        }
+        else{
+           throw "Invalid Date";
+        }        
+    }
+}
